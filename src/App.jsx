7c3236e5f527e89dae31a3ef0e448888cc97f696
Shawon Mahmud <shawon.phero@gmail.com>
2023-09-17 20:44:05 +0600

    course title added in card section and display toast for error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { useState } from "react";
 import CourseBlogs from "./components/CourseBlogs/CourseBlogs";
 import CourseDetailsCard from "./components/CourseDetailsCard/CourseDetailsCard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [courseTitle, setCourseTitle] = useState([]);
 
   const handleClickBtn = (blogs) => {
-    const newCourseTitle = [...courseTitle, blogs];
-    setCourseTitle(newCourseTitle);
+    const isExist = courseTitle.find((item) => item.id == blogs.id);
+    if (isExist) {
+      return toast.error("Already added");
+    } else {
+      const newCourseTitle = [...courseTitle, blogs];
+      setCourseTitle(newCourseTitle);
+    }
   };
 
   return (
     <>
-      <div className="max-w-7xl mx-auto mt-10">
+      <div className="max-w-7xl mx-auto pt-10">
+        <ToastContainer></ToastContainer>
         {/*Course Title */}
         <h1 className="text-center font-extrabold text-[#1C1B1B] text-3xl">
           Course Registration
